f098a354a00fee1bc75fa1ce27c3a212f3e7d12b
JasielAaron <150881134+JasielAaron@users.noreply.github.com>
2024-01-15 14:38:34 -0700

    added arrays to js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//4 Arrays of characters 
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTVUWXYZ' .split("")
+var lowercase = 'abcedfghijklmnopqrstuvwxyz' .split("")
+var numbers = '1234567890' .split("")
+var specialsm ='@!#$%^&*' .split("")
+
+var isUppercase = confim("D you want uppercase letters")
+var isLowercase = confim("D you want lowercase letters")
+var isNumbers = confim("D you want numbers letters")
+var isSpecails = confim("D you want specail characters")
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
